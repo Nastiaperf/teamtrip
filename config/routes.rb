c3d633a70d2e6f5f00b8e8310b67edccf6ca7cf0
Vincent Vignali <vincent.vignali@hotmail.fr>
2021-11-29 18:03:15 +0100

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :user, only:[:new, :create]
+
+  resources :trips, only: [:new, :create, :show, :update] do
+    resources :days, only: [:index, :show]
+  end
+
+  resources :suggestions, only: [:new, :create]
+  resources :votes, only: [:create, :destroy]
+
 end
