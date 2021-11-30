d41cd1d4cb3515e4273f5ee27687d6ceca17b2fb
romain plantey <plantey.romain@gmail.com>
2021-11-30 18:33:27 +0100

    trip and days controllers added (still to be reviewed)

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,7 +1,12 @@
 class DaysController < ApplicationController
   def index
+    #days of the trip page
+    @days = Day.all
   end
 
   def show
+    #details of the suggestions of the day
+    @day = Day.find(params[:id])
   end
+
 end
