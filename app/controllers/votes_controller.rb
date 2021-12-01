af28c7e65ed82eddf95e06943071aa73ea27173a
Vincent Vignali <vincent.vignali@hotmail.fr>
2021-12-01 12:45:29 +0100

    controllers votes and suggestions

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,14 @@
 class VotesController < ApplicationController
   def create
+    suggestion = Suggestion.find(params[:suggestion_id])
+    new_vote = Vote.new(suggestion: suggestion, user: current_user)
+    new_vote.save!
+    # REDIRECT
   end
 
   def destroy
+    vote = Vote.find(param[:vote_id])
+    vote.destroy!
+    # REDIRECT
   end
 end
