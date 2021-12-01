3b0b8c31e23f5114ce174b250d3619c147fc26b8
Vincent Vignali <vincent.vignali@hotmail.fr>
2021-12-01 17:29:21 +0100

    tab menu html without js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,3 +32,13 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
