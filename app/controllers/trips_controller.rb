551b8630ec4bcb7f053064242c8ff483525f22a1
camille martin <camillemartin4497@gmail.com>
2021-12-06 13:09:31 +0100

    end calendar

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -11,7 +11,24 @@ class TripsController < ApplicationController
 
   def show
     #roadmap page
-    @trip = Trip.find(params[:id])
+    @trip        = Trip.find(params[:id])
+    @trip_days   = @trip.days.order(:date)
+    @current_day = @trip_days.find_by(date: params[:date]) || @trip_days.first
+
+    @current_date = (@current_day.date).strftime("%Y-%m-%d")
+    
+    unless @current_day == @trip_days.last
+      @next_date = (@current_day.date + 1.day).strftime("%Y-%m-%d")
+    end
+
+    unless @current_day == @trip_days.first
+      @previous_date = (@current_day.date - 1.day).strftime("%Y-%m-%d")
+      @previous_hotel = @trip_days.find_by(date: @previous_date).suggestions.
+                          where(category: "Hotel").
+                          first
+    end
+    # @current_hotel =
+    # @current_activities =
   end
 
   def update
