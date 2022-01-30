5513ebf5025d77f0597b259efc93671e07f5ccc2
Anastasia Parfait <anastasia.parfait@audencia.com>
2022-01-30 18:00:52 +0100

    Creation of routes, def and view index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :trips, only: [:new, :create, :show, :update] do
+  resources :trips, only: [:new, :create, :show, :update, :index] do
     resources :days, only: [:index, :show]
     resources :trip_members, only: [:new, :create]
     member do
